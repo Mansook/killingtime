5783a90ab424b23c1050eee3c0d7a6895202bd44
Mansook <orgin20@naver.com>
2022-10-18 16:33:43 +0900

    v 1.0.0

--- voca_backend/src/api/index.js
+++ voca_backend/src/api/index.js
@@ -3,11 +3,9 @@ const Router = require("koa-router");
 const api = new Router();
 const wordlist = require("./wordlist");
 const booklist = require("./booklist");
-const papago = require("./papago");
 const test = require("./testlist");
 
 api.use("/words", wordlist.routes());
 api.use("/books", booklist.routes());
-api.use("/papago", papago.routes());
 api.use("/test", test.routes());
 module.exports = api;
--- voca_backend/src/api/papago/index.js
+++
@@ -1,8 +0,0 @@
-const Router = require("koa-router");
-
-const papago = new Router();
-const papagoCtrl = require("./papago.controller");
-
-papago.get("/translate", papagoCtrl.translate);
-
-module.exports = papago;
